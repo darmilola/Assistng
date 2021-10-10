5243136153f9ec12f534c671153c16567c46d97a
= <=>
2021-10-10 19:37:37 +0100

    Integrated Room

--- app/src/main/java/ng/assist/UIs/ViewModel/TransactionDatabase.kt
+++ app/src/main/java/ng/assist/UIs/ViewModel/TransactionDatabase.kt
@@ -3,7 +3,7 @@ package ng.assist.UIs.ViewModel
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = arrayOf(User::class), version = 1)
+@Database(entities = arrayOf(Transactions::class), version = 1)
 abstract class TransactionDatabase : RoomDatabase() {
     abstract fun transactionDao():TransactionDao;
 }
